5fb5624c378b3814b0177ea589e79534cfc12a6d
shaohua.wangshaohu <shaohua.wangshaohu@alibaba-inc.com>
2017-04-26 19:16:55 +0800

    用户信息注册

--- ElementWarService/src/main/java/com/artisankid/elementwar/tcpconnection/action/impl/Login.java
+++ ElementWarService/src/main/java/com/artisankid/elementwar/tcpconnection/action/impl/Login.java
@@ -7,6 +7,9 @@ import com.artisankid.elementwar.ewmessagemodel.LoginMessageOuterClass.LoginMess
 import com.artisankid.elementwar.ewmessagemodel.LoginNoticeOuterClass.LoginNotice;
 import com.artisankid.elementwar.tcpconnection.annotations.ActionRequestMap;
 import com.artisankid.elementwar.tcpconnection.annotations.NettyAction;
+import com.artisankid.elementwar.tcpconnection.client.Client;
+import com.artisankid.elementwar.tcpconnection.gate.utils.ClientConnection;
+import com.artisankid.elementwar.tcpconnection.gate.utils.ClientConnectionMap;
 import io.netty.channel.ChannelHandlerContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -35,6 +38,15 @@ public class Login {
         TokenDao dao = new TokenDao();
         if(dao.selectByAccessToken(accessToken) != null) {
             //TODO:将userID和ctx进行绑定
+            ClientConnectionMap.addClientConnection(ctx);
+            ClientConnection conn = ClientConnectionMap.getClientConnection(ctx);
+            ClientConnectionMap.registerUserId(userID, conn.getNetId());
+
+
+            //TODO 获取逻辑根据用户Id获取对应的连接从而获取上下文
+            Long netId = ClientConnectionMap.userid2netid(userID);
+            ClientConnection clientConnection = ClientConnectionMap.getClientConnection(netId);
+            ChannelHandlerContext returnCtx = clientConnection.getChannelHandlerContext();
 
             LoginNotice.Builder notice = LoginNotice.newBuilder();
             notice.setMessageId(messageID);
